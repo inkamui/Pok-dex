0ba55e2177e52a236c3733f05ca8db5814f6b79c
inkamui <kamuix10@gmail.com>
2023-05-03 21:02:43 -0300

    New

--- js/script.js
+++ js/script.js
@@ -23,20 +23,20 @@ const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = 'Loading ...';
   pokemonNumber.innerHTML = '';
 
-  const data = await fetchPokemon(pokemon);
+const data = await fetchPokemon(pokemon);
 
 if (data) {
-    pokemonImage.style.display = 'block';
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value ='';
-    searchPokemon = data.id;
+  pokemonImage.style.display = 'block';
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  input.value ='';
+  searchPokemon = data.id;
 } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not Found';
-    pokemonNumber.innerHTML = '';
-  }
+  pokemonImage.style.display = 'none';
+  pokemonName.innerHTML = 'Not Found';
+  pokemonNumber.innerHTML = '';
+ }
 }
 
 form.addEventListener('submit', (event) => {
